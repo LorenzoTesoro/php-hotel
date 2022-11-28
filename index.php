5be00d157bba92017418253888960f78c71f9abd
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-28 15:01:59 +0100

    add hotels table

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         ],
 
     ];
-
+// Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
 ?>
 
 <!DOCTYPE html>
@@ -49,14 +49,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <style>
+        .table-responsive{
+            max-width: 1200px;
+            margin: 1rem auto;
+        }
+    </style>
 </head>
 <body>
-       <ul>
-            <?php foreach ($hotels as $hotel) : ?>
-            <li>
-                <?php echo $hotel['name'] . ' ' . $hotel['description'] . ' ' . $hotel['parking'] . ' ' . $hotel['vote'] . ' ' . $hotel['distance_to_center'] ?>
-            </li>
-            <?php endforeach ?>
-       </ul>
+        <h1 class="text-center">Hotels</h1>
+       <div class="table-responsive">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to Center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr class="">
+                        <td scope="row"><?php echo $hotel['name'] ?></td>
+                        <td><?php echo $hotel['description'] ?></td>
+
+                        <?php if($hotel['parking'] === true) :?>
+                        <td><?php echo 'Yes' ?></td>
+                        <?php else : ?>
+                        <td><?php echo 'No' ?></td>
+                        <? endif ?>
+                        <td><?php echo $hotel['vote'] ?></td>
+                        <td><?php echo $hotel['distance_to_center'] ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+       </div>
 </body>
 </html>
