f31139ea1bb60cf843b00fda3e1c8cdabf176d53
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-28 17:39:43 +0100

    add bonus: filter by parking

--- index.php
+++ index.php
@@ -1,49 +1,79 @@
 <?php
-// Stampare tutti i nostri hotel con tutti i dati disponibili.
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => 'true',
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => 'true',
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => 'false',
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => 'false',
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => 'true',
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
-// Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+];
+
+var_dump($_GET);
+
+if (empty($_GET)) {
+    $hotels;
+} else {
+    $filtered_array = [];
+
+    foreach ($hotels as $hotel) {
+        if ($_GET['parking'] === $hotel['parking']) {
+            $filtered_array[] = $hotel;
+        }
+    }
+
+    $hotels = $filtered_array;
+
+    var_dump($hotels);
+}
+
+
+
+
+
 ?>
+
+<!-- 
+    Bonus: 1
+     - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+     - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
+ -->
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -54,15 +84,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <style>
-        .table-responsive{
+        .table-responsive {
             max-width: 1200px;
             margin: 1rem auto;
         }
     </style>
 </head>
+
 <body>
-        <h1 class="text-center">Hotels</h1>
-       <div class="table-responsive">
+    <h1 class="text-center">Hotels</h1>
+    <form action="index.php" method="get">
+        <div>
+            <label for="parking">Parking:</label>
+            <select name="parking" id="parking">
+                <option value="" disabled='disabled' selected>Choose an option</option>
+                <option value=true>Yes</option>
+                <option value=false>No</option>
+            </select>
+        </div>
+        <div>
+            <label for="vote">Vote:</label>
+            <select name="vote" id="vote">
+                <option value="" disabled='disabled' selected>Choose a vote</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+        </div>
+        <button type='submit'>Submit</button>
+    </form>
+
+    <div class="table-responsive">
         <table class="table table-primary">
             <thead>
                 <tr>
@@ -79,17 +133,18 @@
                         <td scope="row"><?php echo $hotel['name'] ?></td>
                         <td><?php echo $hotel['description'] ?></td>
 
-                        <?php if($hotel['parking'] === true) :?>
-                        <td><?php echo 'Yes' ?></td>
+                        <?php if ($hotel['parking'] === 'true') : ?>
+                            <td><?php echo 'Yes' ?></td>
                         <?php else : ?>
-                        <td><?php echo 'No' ?></td>
+                            <td><?php echo 'No' ?></td>
                         <? endif ?>
                         <td><?php echo $hotel['vote'] ?></td>
                         <td><?php echo $hotel['distance_to_center'] ?></td>
                     </tr>
-                <?php endforeach ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
-       </div>
+    </div>
 </body>
+
 </html>
